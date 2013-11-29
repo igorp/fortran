05f104d90d074bdac0eb61bcde5b8adafc177a40
Igor <Igor@192.168.1.35>
2013-11-29 12:36:13 +0200

    Game of Life works now.

--- src/Life.java
+++ src/Life.java
@@ -2,113 +2,94 @@
 public class Life {
 
     static int SIZE = 10;
-    static int ITERATIONS = 15;
+    static int ITERATIONS = 10;
     static boolean[][] grid = new boolean[SIZE][SIZE];
 
     public static void main(String args[]) throws InterruptedException {
 
         boolean[][] originalGrid = new boolean[SIZE][SIZE];
 
-        
-        grid[1][2] = true;
-        grid[2][1] = true;
-        grid[2][2] = true;
-        grid[3][2] = true;
-        grid[2][3] = true;
-        //grid[3][3] = true;
-        grid[5][5] = true;
+        //set the first generation shape
         grid[4][5] = true;
+        grid[5][4] = true;
+        grid[5][5] = true;
+        grid[5][6] = true;
+        grid[6][5] = true;
 
-        //grid[0][2] = true;
-       // grid[1][1] = true;
-       // grid[1][3] = true;
-       // grid[2][0] = true;
-        
-        
-        
         for (int n = 0; n < ITERATIONS; n++) {
             printGrid();
             copyArray(grid, originalGrid);
             System.out.println();
-            Thread.sleep(1000);
+            Thread.sleep(500);
 
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid.length; j++) {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
                     grid[i][j] = nextState(i, j, originalGrid);
                 }
             }
         }
     }
 
-    static boolean nextState(int i, int j, boolean[][] grid_orig) {
-        //count how many neigbours cell has
+    static boolean nextState(int i, int j, boolean[][] grid) {
         int neighbours = 0;
 
         //check perpendicular cells
-        if (i + 1 < SIZE && grid_orig[i + 1][j]) {
+        if (i + 1 < SIZE && grid[i + 1][j]) {
             neighbours++;
         }
-        if (i - 1 > -1 && grid_orig[i - 1][j]) {
+        if (i - 1 > -1 && grid[i - 1][j]) {
             neighbours++;
         }
-        if (j + 1 < SIZE && grid_orig[i][j + 1]) {
+        if (j + 1 < SIZE && grid[i][j + 1]) {
             neighbours++;
         }
-        if (j - 1 > -1 && grid_orig[i][j - 1]) {
+        if (j - 1 > -1 && grid[i][j - 1]) {
             neighbours++;
         }
 
         //check diagonal cells
-        if (i + 1 < SIZE && j + 1 < SIZE && grid_orig[i + 1][j + 1]) {
+        if (i + 1 < SIZE && j + 1 < SIZE && grid[i + 1][j + 1]) {
             neighbours++;
         }
-        if (i - 1 > -1 && j - 1 > -1 && grid_orig[i - 1][j - 1]) {
+        if (i - 1 > -1 && j - 1 > -1 && grid[i - 1][j - 1]) {
             neighbours++;
         }
-        if (i + 1 < SIZE && j - 1 > -1 && grid_orig[i + 1][j - 1]) {
+        if (i + 1 < SIZE && j - 1 > -1 && grid[i + 1][j - 1]) {
             neighbours++;
         }
-        
-       // if(i == 1 && j == 1) {
-        //    System.out.println("i - 1 > 0: " + (i - 1 > 0));
-        //}
-        if (i - 1 > -1 && j + 1 < SIZE && grid_orig[i - 1][j + 1]) {
+        if (i - 1 > -1 && j + 1 < SIZE && grid[i - 1][j + 1]) {
             neighbours++;
-         //   System.out.println(i + " " + j + "HERE&");
-        }
-        if (grid[i][j]) {
-       //     System.out.println(i + " " + j + " neighbours: " + neighbours);
         }
 
-        if (grid_orig[i][j] && neighbours < 2) {
+        if (grid[i][j] && neighbours < 2) {
             return false;
         }
-        //probably not needed
-        if (grid_orig[i][j] && (neighbours == 2 || neighbours == 3)) {
+        if (grid[i][j] && (neighbours == 2 || neighbours == 3)) {
             return true;
         }
-        if (grid_orig[i][j] && neighbours > 3) {
+        if (grid[i][j] && neighbours > 3) {
             return false;
         }
-        if (!grid_orig[i][j] && neighbours == 3) {
+        if (!grid[i][j] && neighbours == 3) {
             return true;
         }
         return false;
     }
 
     static void printGrid() {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 if (grid[i][j]) {
-                    System.out.print("X ");
-                } else {
                     System.out.print("O ");
+                } else {
+                    System.out.print("  ");
                 }
             }
             System.out.println();
         }
     }
 
+    //copy array from a to b
     static void copyArray(boolean[][] a, boolean[][] b) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
