9eb390c2f9671816388a1e8aa81c9da56d23b789
igorp <igor.podsechin@gmail.com>
2017-11-14 01:20:04 +0200

    Update binary_tree.py

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -132,7 +132,131 @@ class Tree:
                 inOrder(localRoot.rightChild)
 
         inOrder(self.root)
+        
+    # A rather ugly function that draws a tree up to 4 levels
+    def draw(self):
+            treeFlattened = [-99] * 15
+            a = self.root
 
+            if(a != None):
+                treeFlattened.pop(0)
+                treeFlattened.insert(0, a.data)
+                b = a.leftChild
+                c = a.rightChild
+
+                if(b != None):
+                    treeFlattened.pop(1)
+                    treeFlattened.insert(1, b.data)
+                    d = b.leftChild
+                    e = b.rightChild
+
+                    if(d != None):
+                        treeFlattened.pop(3)
+                        treeFlattened.insert(3, d.data)
+                        h = d.leftChild
+                        i = d.rightChild
+
+                        if(h != None):
+                            treeFlattened.pop(7)
+                            treeFlattened.insert(7, h.data)   
+
+                        if(i != None):
+                            treeFlattened.pop(8)
+                            treeFlattened.insert(8, i.data)     
+
+                    if(e != None):
+                        treeFlattened.pop(4)
+                        treeFlattened.insert(4, e.data)
+                        j = e.leftChild
+                        k = e.rightChild
+
+                        if(j != None):
+                            treeFlattened.pop(9)
+                            treeFlattened.insert(9, j.data)   
+
+                        if(k != None):
+                            treeFlattened.pop(10)
+                            treeFlattened.insert(10, k.data)     
+
+                if(c != None):
+                    treeFlattened.pop(2)
+                    treeFlattened.insert(2, c.data)
+                    f = c.leftChild
+                    g = c.rightChild
+
+                    if(f != None):
+                        treeFlattened.pop(5)
+                        treeFlattened.insert(5, f.data)
+                        l = f.leftChild
+                        m = f.rightChild
+
+                        if(l != None):
+                            treeFlattened.pop(11)
+                            treeFlattened.insert(11, l.data)   
+
+                        if(m != None):
+                            treeFlattened.pop(12)
+                            treeFlattened.insert(12, m.data)     
+
+                    if(g != None):
+                        treeFlattened.pop(6)
+                        treeFlattened.insert(6, g.data)
+                        n = g.leftChild
+                        o = g.rightChild
+
+                        if(n != None):
+                            treeFlattened.pop(13)
+                            treeFlattened.insert(13, n.data)   
+
+                        if(o != None):
+                            treeFlattened.pop(14)
+                            treeFlattened.insert(14, o.data)     
+
+            # draw function's inner helper function
+            def formatNodeData(n):
+                if(n == -99):
+                    return "   "
+                elif(n > 9):
+                    return " " + str(n)
+                else:
+                    return " " + str(n) + " "
+
+            print("        ______" 
+                + formatNodeData(treeFlattened[0]) 
+                + "______ ")
+            print("       /               \\")
+            print("    __" + formatNodeData(treeFlattened[1])
+                + "__         __"
+                + formatNodeData(treeFlattened[2]) + "__")
+            print("   /       \       /       \\")
+            print("  " 
+                + formatNodeData(treeFlattened[3]) 
+                + "     " 
+                + formatNodeData(treeFlattened[4]) 
+                + "     " 
+                + formatNodeData(treeFlattened[5]) 
+                + "     " 
+                + formatNodeData(treeFlattened[6]))
+            print(" /   \   /   \   /   \   /   \\")
+            print("" 
+                + formatNodeData(treeFlattened[7]) 
+                + " " 
+                + formatNodeData(treeFlattened[8]) 
+                + " " 
+                + formatNodeData(treeFlattened[9]) 
+                + " " 
+                + formatNodeData(treeFlattened[10]) 
+                + " " 
+                + formatNodeData(treeFlattened[11]) 
+                + " " 
+                + formatNodeData(treeFlattened[12]) 
+                + " " 
+                + formatNodeData(treeFlattened[13]) 
+                + " " 
+                + formatNodeData(treeFlattened[14]) 
+                + "")
+
+# test out a inserting, deleting and
 def main():    
     
     tree = Tree()
@@ -142,10 +266,11 @@ def main():
     tree.insert(75)
     tree.insert(12)
     tree.insert(37)
+    tree.insert(42)
 
-    tree.traverse()
+    tree.draw()
 
-    tree.delete(12)
+    tree.delete(42)
     tree.traverse()    
 
 main()
